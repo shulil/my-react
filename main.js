831be9dd133b991068c10eae98844559e0623d11
stephan <286901453@qq.com>
2020-12-01 16:47:41 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,26 @@ for(let i of [1,2,3] ) {
     console.log(i)
 }
 class MyComponent extends Component{
+    constructor(){
+        super();
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
    render() {
-       return <div>
-           <h1>my component</h1>
+       return <div class="b">
+           <h1 class="c">my component</h1>
+           <button onClick={()=>{this.setState({a:this.state.a+1}) }}>add</button>
+           <span>{this.state.a.toString()}</span>
+           <span>{this.state.b.toString()}</span>
            {this.children}
            </div>
    }
 }
 render(<MyComponent>
-    <div>234</div>
-    <div></div>
+    <div class="a">234</div>
+    <div>
+        <h1>bumingbai!!!</h1>
+    </div>
 </MyComponent>,document.body)
