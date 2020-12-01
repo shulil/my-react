ead9dcb8aa2f5933748239b4612df57baac746e4
shulil <286901453@qq.com>
2020-12-01 17:02:47 +0800

    test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {createElement, Component, render} from './toy-react.js';
-for(let i of [1,2,3,4,5]) {
+for(let i of [1,2,3,4,5,6]) {
     console.log(i)
 }
 class MyComponent extends Component{
